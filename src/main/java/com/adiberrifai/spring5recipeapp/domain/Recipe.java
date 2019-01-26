4dc46b78a9fa88f1ea7c81f36c71d27dd372b057
Adib Err <adib.err@hotmail.com>
2019-01-26 02:06:45 +0000

    Fix description length limitation.

--- src/main/java/com/adiberrifai/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/adiberrifai/spring5recipeapp/domain/Recipe.java
@@ -1,6 +1,7 @@
 package com.adiberrifai.spring5recipeapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -16,13 +17,15 @@ public class Recipe {
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
-    private Set<Ingredient> ingredients;
+    private Set<Ingredient> ingredients = new HashSet<>();
 
     @Lob
     private Byte[] image;
@@ -30,7 +33,7 @@ public class Recipe {
     @ManyToMany
     @JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"),
     inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private Set<Category> categories;
+    private Set<Category> categories = new HashSet<>();
 
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
@@ -129,5 +132,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
