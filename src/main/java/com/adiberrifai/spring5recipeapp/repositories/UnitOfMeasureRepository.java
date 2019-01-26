b40e70b5375c7f87eaf5645e46879ac4ee52feeb
Adib Err <adib.err@hotmail.com>
2019-01-26 00:01:03 +0000

    added query methods to get entity by description.

--- src/main/java/com/adiberrifai/spring5recipeapp/repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/adiberrifai/spring5recipeapp/repositories/UnitOfMeasureRepository.java
@@ -4,6 +4,10 @@ import com.adiberrifai.spring5recipeapp.domain.UnitOfMeasure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UnitOfMeasureRepository extends JpaRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
