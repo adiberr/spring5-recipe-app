285d2bae302100fefa3cdafc2e14e82b57bcfffb
Adib Err <adib.err@hotmail.com>
2019-01-28 07:28:36 +0000

    refactor for project lombok

--- src/main/java/com/adiberrifai/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/com/adiberrifai/spring5recipeapp/controllers/IndexController.java
@@ -1,10 +1,14 @@
 package com.adiberrifai.spring5recipeapp.controllers;
 
 import com.adiberrifai.spring5recipeapp.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -15,7 +19,9 @@ public class IndexController {
     }
 
     @RequestMapping({"", "/", "/index"})
-    public String getIndexPage(Model model) {
+    public String getIndexPage(Model model, HttpServletRequest request) {
+
+        log.debug("GET /" + request.getRequestURI());
 
         model.addAttribute("recipes", recipeService.getRecipes());
 
--- src/main/java/com/adiberrifai/spring5recipeapp/domain/Notes.java
+++ src/main/java/com/adiberrifai/spring5recipeapp/domain/Notes.java
@@ -1,7 +1,12 @@
 package com.adiberrifai.spring5recipeapp.domain;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 import javax.persistence.*;
 
+@Data
+@EqualsAndHashCode(exclude = "recipe")
 @Entity
 public class Notes {
 
@@ -23,19 +28,4 @@ public class Notes {
         Id = id;
     }
 
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
